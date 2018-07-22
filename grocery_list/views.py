1c90c7fbdec2506760f87d0b5b5dad1072c356c7
reptation <david@musicalquilt.com>
2018-07-22 21:24:12 +0000

    initial template working

--- grocery_list/views.py
+++ grocery_list/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Item
 
 def index(request):
-    return HttpResponse("Welcome to your Grocery List")
+    item_list = Item.objects.all()
+    template = loader.get_template('grocery_list/index.html')
+    context = {
+        'item_list': item_list,
+    }
+
+    return HttpResponse(template.render(context, request))
 
     
 def detail(request, item_id):
